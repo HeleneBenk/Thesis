d2b9f559fae68703f1fcd57c51c58e1680d43f20
HeleneBenk <benkert.helene@web.de>
2025-07-03 23:16:39 +0200

    Create train.sh

---
+++ ViT/HPC Input/train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=ViLTV1
+#SBATCH --mail-type=all              
+#SBATCH --mail-user=benkehel@students.zhaw.ch
+#SBATCH --nodes=1                    
+#SBATCH --ntasks=1                   
+#SBATCH --cpus-per-task=32            
+#SBATCH --time=4-00:00:00
+#SBATCH --mem=64GB                   
+#SBATCH --partition=earth-3
+
+
+module load USS/2022
+module load gcc/9.4.0-pe5.34
+module load miniconda3/4.12.0
+module load lsfm-init-miniconda/1.0.0
+module load cuda/11.6.2
+conda activate myenv
+
+python /cfs/earth/scratch/benkehel/ViT/V1/ViT_V1_Train.py
